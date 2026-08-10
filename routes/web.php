94d14d129cb9efc79df95882bc72e210d3c57a5e
RidoIzril <ridoizril@gmail.com>
2026-08-10 22:24:39 +0700

    Add Vercel diagnostic route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\ProjectController;
 
+Route::get('/test-vercel', function () {
+    return response()->json([
+        'status' => 'Laravel is running',
+        'php' => PHP_VERSION,
+        'app_key' => !empty(config('app.key')),
+        'base_path' => base_path(),
+        'public_path' => public_path(),
+        'manifest_exists' => file_exists(public_path('build/manifest.json')),
+    ]);
+});
 Route::get('/',[PortfolioController::class,'index']);
 Route::get('/project/{slug}', [ProjectController::class, 'show'])->name('project.detail');

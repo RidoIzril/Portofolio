38be78c342fc90aa0b2874ef2af68c23ad6fe9ba
RidoIzril <ridoizril@gmail.com>
2026-08-10 22:13:30 +0700

    Fix Laravel Vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,7 +1,11 @@
 <?php
 
-/**
- * Here is the serverless function entry
- * for deployment with Vercel.
- */
-require __DIR__.'/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
